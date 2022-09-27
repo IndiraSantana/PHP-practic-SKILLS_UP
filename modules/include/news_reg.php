09f0a75b3061b6606d705ec95fb10a10140e3d7e
Indira Santana <indirasantana.pfaeskillsup@gmail.com>
2022-09-27 09:58:34 +0100

    reorganización de carpetas, creación de archivos php, añadir en el action del form el enlace de new_reg

---
+++ modules/include/news_reg.php
@@ -0,0 +1,8 @@
+<?php
+require "../require/config.php"; //Conexión con el fichero config.
+//Definir las variables, El name que le tengo en el formulario (index.html)
+$nombre = $email = $telefono = $direccion = $ciudad = $provincia = $Zip = $check = $formato = $otrostemas = " ";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+}
+?>
