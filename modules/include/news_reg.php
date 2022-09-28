210435eec84b54368d0b744c35e6106ba6523d16
Indira Santana <indirasantana.pfaeskillsup@gmail.com>
2022-09-28 10:30:16 +0100

    Quitar el botón 'enviar' y añadir el placeholder y requerido al 'telefono'

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -3,6 +3,9 @@ require "../require/config.php"; //Conexión con el fichero config.
 //Definir las variables, El name que le tengo en el formulario (index.html)
 $nombre = $email = $telefono = $direccion = $ciudad = $provincia = $Zip = $check = $formato = $otrostemas = " ";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+    echo "<br><strong>Metodo post enviado</strong><br>";
+    if(!empty($__POST["name"]) || !empty($__POST["email"]) || !empty($__POST["telefono"])){
+        
+    }
 }
-?>
+?>   
